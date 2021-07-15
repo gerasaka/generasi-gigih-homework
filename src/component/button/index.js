dbd9603e399cd878dc0d8a404903b02303ec1197
gerasaka <gerryjulioo@gmail.com>
2021-07-16 03:37:53 +0700

    import style module

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,5 +1,7 @@
+import style from "./style.module.css";
+
 const Button = props => {
-    return <button>clickme</button>;
+    return <button className={style.btn}>Select</button>;
 }
 
 export default Button;
