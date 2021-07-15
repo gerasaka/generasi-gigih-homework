a3cacf1eb68f5aea32ae765f071cf72ef720e24b
gerasaka <gerryjulioo@gmail.com>
2021-07-15 19:21:22 +0700

    import content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from "./component/content"
+import data from "./data/apiData.js"
+// import detail from "./data/apiCall.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Playlist</h1>
+      <Content data={data}/>
+      <content />
     </div>
   );
 }
