82b0d1be30ac65c538f0affc4ce1d97ae3301241
gerasaka <gerryjulioo@gmail.com>
2021-07-17 18:40:30 +0700

    refactor button component to reusable component

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,7 +1,7 @@
 import style from "./style.module.css";
 
 const Button = props => {
-    return <button className={style.btn}>Select</button>;
+    return <button className={style.btn}>{props.button}</button>;
 }
 
 export default Button;
