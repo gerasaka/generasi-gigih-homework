751c3ba3492a89c494787966e7082ee6acfa3774
gerasaka <gerryjulioo@gmail.com>
2021-07-17 18:44:24 +0700

    simplify content component

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -1,11 +1,10 @@
-import data from "../../data/apiData";
 import Card from "../card";
 
 const Content = () => {
     return (
     <div>
         <h1>Create Playlist</h1>
-        <Card data={data} />
+        <Card />
     </div>
     );
 }
