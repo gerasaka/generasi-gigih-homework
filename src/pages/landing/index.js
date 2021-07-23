ae74a7b42779001e753cecf1ed37c15e9f34642f
gerasaka <gerryjulioo@gmail.com>
2021-07-23 08:53:41 +0700

    create a page to redirect to spotify auth

---
+++ src/pages/landing/index.js
@@ -0,0 +1,11 @@
+const Landing = () => {
+    return(
+        <>
+        <h1>Welcome Fellas</h1>
+        <p>To continue, log in with Spotify</p>
+        <button><span>logo</span>Log in</button>
+        </>
+    );
+}
+
+export default Landing;
