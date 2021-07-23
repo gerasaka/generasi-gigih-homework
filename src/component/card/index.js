e3e8ebca0381e3dc537d4692c6269564de640a3b
gerasaka <gerryjulioo@gmail.com>
2021-07-23 16:05:29 +0700

    change props call

--- src/component/card/index.js
+++ src/component/card/index.js
@@ -3,26 +3,15 @@ import Detail from "../detail";
 import Button from "../button";
 
 import style from "./style.module.css";
-import data from "../../data/apiData";
 
-const Card = props => {
-    return data.map( data =>
+const Card = (props) => {
+    return (
         <div className={style.card}>
-            <AlbumImg src={data.album.images[2].url} alt={data.album.name} />
-            <Detail title={data.name} artists={data.artists[0].name} album={data.album.name} />
-            <Button link={data.external_urls} button={"Select"} />
+            <AlbumImg src={props.image}/>
+            <Detail title={props.title} artists={props.artist} album={props.album} />
+            <Button link={props.url} label={"Select"} />
         </div>
     );
-
-    // const { name, artists, album, external_urls } = data[1];
-    
-    // return (
-    //     <div className={style.card}>
-    //         <AlbumImg src={album.images[2].url} alt={album.name} />
-    //         <Detail title={name} artists={artists[0].name} album={album.name} />
-    //         <Button link={external_urls} button={"Select"} />
-    //     </div>
-    // );
 }
 
 export default Card;
