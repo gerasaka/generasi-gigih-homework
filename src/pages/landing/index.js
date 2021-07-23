3a13a0ffd280739d4aaaf371452612c9984057a8
gerasaka <gerryjulioo@gmail.com>
2021-07-23 10:41:51 +0700

    refactor landing page to use state

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,11 +1,32 @@
-const Landing = () => {
-    return(
-        <>
-        <h1>Welcome Fellas</h1>
-        <p>To continue, log in with Spotify</p>
-        <button><span>logo</span>Log in</button>
-        </>
-    );
-}
+import { useState, useEffect } from "react";
+import Button from "../../component/button"
+import Search from "../../component/search"
 
-export default Landing;
+export default function Landing(props) {
+  const [isLogged, setLogin] = useState(false);
+
+  useEffect(() => {
+    if (window.location.hash) {
+      setLogin(true);
+    }
+  }, []);
+
+  return (
+    <div>
+      {
+        isLogged ? (
+            <Search handleSubmit={props.handleSearch} />
+        ) : (
+          <>
+            <h1>Welcome Fellas</h1>
+            <p>To continue, log in with Spotify</p>
+            <Button
+              onClick={props.handleAuth}
+              label="Login"
+            />
+          </>
+        )
+      }
+    </div>
+  );
+}
