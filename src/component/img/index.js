2dd14e34a0e69bf0a919991e98618803d266b08d
gerasaka <gerryjulioo@gmail.com>
2021-07-23 16:05:57 +0700

    change props call

--- src/component/img/index.js
+++ src/component/img/index.js
@@ -1,7 +1,17 @@
 import style from "./style.module.css";
 
 const AlbumImg = (props) => {
-    return <img className={style.img} src={props.src} alt={props.alt} />
+    return (
+        <div className={style.container}>
+            <img className={style.image} src={props.src} alt={props.alt} />
+            {/* <div className={style.overlay}>
+                <a href={props.link} className={style.icon}>
+                    <img src="/play-button.svg"/>
+                    <i>Play on Spotify</i>
+                </a>
+            </div> */}
+        </div>
+    );
 }
 
 export default AlbumImg;
