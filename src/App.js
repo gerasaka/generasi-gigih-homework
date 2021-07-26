4f11ed3aeebc62cbc4ad8ea4322e3e27c272c99f
gerasaka <gerryjulioo@gmail.com>
2021-07-26 15:19:51 +0700

    refactor app to make it has conditional render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Content from './pages/create-playlist';
+import { useState, useEffect } from 'react';
+import { handleAuth, getTokenFromUrl } from './functions/auth';
+
+import Home from './pages/Home';
+import Landing from './pages/landing';
 
 
 function App() {
+  const [isLogged, setLogin] = useState(false);
+  const [Token, setToken] = useState("");
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const accessToken = getTokenFromUrl(window.location.hash);
+      setToken(accessToken);
+      setLogin(true);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <Content />
-    </div>
+      isLogged ? <Home token={Token} /> : <Landing redirect={handleAuth}/>
   );
 }
 
