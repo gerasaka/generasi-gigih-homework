f798f62ebc81e6cdf4a4ca97fff94a8a52618283
gerasaka <gerryjulioo@gmail.com>
2021-08-06 16:45:13 +0700

    use redux to store isLogin state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import { useState, useEffect } from 'react';
 import { handleAuth, getSpotifyToken, getUserData } from './services/spotifyAuth';
 
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 import { useDispatch, useSelector } from 'react-redux';
-import { storeToken } from "./redux/slice"
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 function App() {
-  const [isLogin, setLogin] = useState(false);
-  const [userID, setUserID] = useState("");
-  
-  // const [token, setToken] = useState("");
-  const token = useSelector(state => state.token.value)
-
-  const dispatch = useDispatch();
-
-  
-  useEffect(() => {
-    if (window.location.hash) {
-      const {access_token} = getSpotifyToken(window.location.hash);
-      // setToken(access_token);
-      dispatch(storeToken(access_token));
-      getUserData(access_token).then((data) => setUserID(data.id));
-      setLogin(true);
-    }
-  }, [dispatch]);
+  const { isLogin } = useSelector(state => state.login)
 
   return (
-      // isLogin ? <Home token={token} userID={userID} /> : <Landing redirect={handleAuth}/>
       <Router>
         <Switch>
           <Route path="/create-playlist">
-            {isLogin ? <Home token={token} userID={userID} /> : <Redirect to="/" />}
+            {isLogin ? <Home /> : <Redirect to="/" />}
           </Route>
           <Route path="/">
             {isLogin ? <Redirect to="/create-playlist" /> : <Landing redirect={handleAuth}/>}
           </Route>
+          <Route><h1>404</h1></Route>
         </Switch>
       </Router>
   );
