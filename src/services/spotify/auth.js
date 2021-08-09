2fb62e7c50247923e8e4f0a9401787214368b39a
gerasaka <gerryjulioo@gmail.com>
2021-08-09 16:04:16 +0700

    split handle auth function

---
+++ src/services/spotify/auth.js
@@ -0,0 +1,19 @@
+const handleAuth = () => {
+  const clientId = process.env.REACT_APP_SPOTIFY_ID;
+  const responseType = "token";
+  const redirectUri = "http://localhost:3000";
+  const scope = "playlist-modify-private";
+  const endPoint = "https://accounts.spotify.com/authorize";
+
+  return (window.location = `${endPoint}?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scope}&show_dialog=true`);
+};
+
+const getUserData = (accessToken) => {
+  return fetch("https://api.spotify.com/v1/me", {
+    headers: {
+      Authorization: "Bearer " + accessToken,
+    },
+  }).then((res) => res.json());
+};
+
+export { handleAuth, getUserData };
