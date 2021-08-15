a438f70428149f3abf5c74e160b5bb77513c8443
gerasaka <gerryjulioo@gmail.com>
2021-08-15 07:24:42 +0700

    refactor isLogin state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Landing from './pages/Landing';
 import useAuth from './libraries/checkLogin';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
-
 function App() {
   const { isLogin } = useAuth();
 
--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,17 +1,11 @@
-import Button from "../../components/Button"
 import { handleAuth } from "../../services/spotify/auth";
 
 export default function Landing() {
-
-  const handleClick = () => {
-    return window.location = handleAuth();
-  };
-
   return (
     <div>
       <h1>Welcome Fellas</h1>
       <p>To continue, log in with Spotify</p>
-      <Button label="Login" onClick={handleClick}/>
+      <button onClick={() => window.location = handleAuth()}>Login</button>
     </div>
   );
 }
