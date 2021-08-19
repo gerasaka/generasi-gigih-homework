3c39a568b8575c54dc4779275abb20d93f62fdd3
gerasaka <gerryjulioo@gmail.com>
2021-08-20 04:56:36 +0700

    add footer to landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,15 @@
 import { handleAuth } from "../../services/spotify/auth";
+import Footer from "../../components/layout/Footer"
 
 export default function Landing() {
   return (
     <div>
-      <h1>Welcome Fellas</h1>
-      <p>To continue, log in with Spotify</p>
-      <button onClick={() => window.location = handleAuth()}>Login</button>
+      <div>
+        <h1>Welcome Fellas</h1>
+        <p>To continue, log in with Spotify</p>
+        <button onClick={() => window.location = handleAuth()}>Login</button>
+      </div>
+      <Footer />
     </div>
   );
 }
