7b7b889d3eac5788d691e44e48f288d3bf9eab81
gerasaka <gerryjulioo@gmail.com>
2021-08-20 04:57:54 +0700

    remove unused props

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,11 @@
 import Button from "../Button";
 import useEventHandler from "../../libraries/eventHandler"
 
-const Search = (props) => {
+const Search = () => {
   const {handleSearch} = useEventHandler();
   return (
     <form onSubmit={handleSearch}>
-      <input type="text" name={props.name} placeholder={props.placeholder} />
+      <input type="text" name="query" placeholder="Search track" />
       <Button type="submit" label="Search"/>
     </form>
   );
