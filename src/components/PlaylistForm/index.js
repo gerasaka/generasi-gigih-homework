08d3b15edb54b466b842a935fb6f46b580d68e8e
gerasaka <gerryjulioo@gmail.com>
2021-08-20 06:17:16 +0700

    move playlist form to center

--- src/components/PlaylistForm/index.js
+++ src/components/PlaylistForm/index.js
@@ -1,9 +1,10 @@
 import useEventHandler from "../../libraries/eventHandler";
+import style from "./style.module.css"
 
 const PlaylistForm = () => {
   const {handleFormSubmit, handleChange} = useEventHandler();
   return(
-    <form onSubmit={handleFormSubmit}>
+    <form onSubmit={handleFormSubmit} className={style.container}>
       <label htmlFor="name">Playlist name</label>
       <input
         id="name"
