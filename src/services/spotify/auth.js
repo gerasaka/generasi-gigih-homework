71812e284c303b61e3d1f6edc40d4c76c4d7ad78
gerasaka <gerryjulioo@gmail.com>
2021-08-20 18:02:22 +0700

    change redirect uri

--- src/services/spotify/auth.js
+++ src/services/spotify/auth.js
@@ -1,7 +1,7 @@
 const handleAuth = () => {
   const clientId = process.env.REACT_APP_SPOTIFY_ID;
   const responseType = "token";
-  const redirectUri = "http://localhost:3000";
+  const redirectUri = "https://flowso.vercel.app/";
   const scope = "playlist-modify-private";
   const endPoint = "https://accounts.spotify.com/authorize";
 
