30052cfa9dfeded93cbdbce23eedbc2330cc08ac
gerasaka <gerryjulioo@gmail.com>
2021-08-20 10:16:27 +0700

    add chakra-ui to input filed and textarea

--- src/components/PlaylistForm/index.js
+++ src/components/PlaylistForm/index.js
@@ -1,25 +1,30 @@
 import useEventHandler from "../../libraries/eventHandler";
 import style from "./style.module.css"
+import { Input, Textarea, Button } from "@chakra-ui/react"
 
 const PlaylistForm = () => {
   const {handleFormSubmit, handleChange} = useEventHandler();
   return(
     <form onSubmit={handleFormSubmit} className={style.container}>
-      <label htmlFor="name">Playlist name</label>
-      <input
-        id="name"
-        name="name"
+      <label htmlFor="playlist-name">Playlist name</label>
+      <Input
+        variant="filled"
+        size="sm"
+        id="playlist-name"
+        name="playlist-name"
         minLength="10"
         onChange={handleChange}
       />
       <label htmlFor="description">Description</label>
-      <textarea
+      <Textarea
+        variant="filled"
+        size="sm"
         id="description"
         name="description"
         minLength="20"
         onChange={handleChange}
-      ></textarea>
-      <button type="submit">Create Playlist</button>
+      />
+      <Button type="submit" colorScheme="blue">Create Playlist</Button>
     </form>
   );
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,21 @@
 import Button from "../Button";
-import useEventHandler from "../../libraries/eventHandler"
+import useEventHandler from "../../libraries/eventHandler";
+import { Input } from "@chakra-ui/input";
+import style from "./style.module.css";
 
 const Search = () => {
-  const {handleSearch} = useEventHandler();
+  const { handleSearch } = useEventHandler();
   return (
     <form onSubmit={handleSearch}>
-      <input type="text" name="query" placeholder="Search track" />
-      <Button type="submit" label="Search"/>
+      <Input
+        className={style.input}
+        type="text"
+        name="query"
+        placeholder="Search track"
+      />
+      <Button type="submit" label="Search" />
     </form>
   );
-}
+};
 
 export default Search;
