ba2d51fe55b6406a68c48edeea0afb85acebf101
gerasaka <gerryjulioo@gmail.com>
2021-08-23 05:52:22 +0700

    add style to header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,12 +1,29 @@
-import "./style.module.css"
+import style from "./style.module.css";
+import { Link } from "react-router-dom"
+import { Avatar } from "@chakra-ui/react";
 
 const Header = () => {
-  return(
+  return (
     <header>
-      <div>Logo</div>
-      <div>Account</div>
+      <div className={style.logo}>Flowso</div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link className={style.navItem} to="/create-playlist">Create Playlist</Link>
+          </li>
+          <li>
+            <Link className={style.navItem} to="/your-playlist">Your Playlist</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={style.avatar}>
+        <p>Your Name</p>
+        <Avatar name="Oshigaki Kisame" src="https://bit.ly/dan-abramov" />
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
